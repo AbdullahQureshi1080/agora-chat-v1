ec7c2abdd89791bd870bc3f1c5253572e79ce7b2
Snapthat Technologies (Private) Limited <snapthat@Jamalui-MacBookPro.local>
2024-02-19 12:38:32 +0500

    feat: initial-setup

---
+++ Config.ts
@@ -0,0 +1,6 @@
+import Config from 'react-native-config';
+
+export const AppConfig = {
+  APP_ID: Config.APP_ID,
+  TOKEN: Config.TOKEN,
+};
---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Snapthat Technologies (Private) Limited on 15/02/2024.
+//
+
+import Foundation
